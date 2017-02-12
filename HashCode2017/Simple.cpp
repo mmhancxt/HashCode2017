0b9b8b0590e910e3dad7cdbbe52ef817adb2929f
mmhancxt@gmail.com <mmhancxt@gmail.com>
2017-02-12 16:03:50 +0100

    asdf

---
+++ HashCode2017/Simple.cpp
@@ -0,0 +1,14 @@
+//
+//  Simple.cpp
+//  HashCode2017
+//
+//  Created by Liang LIU on 12/02/2017.
+//  Copyright © 2017 Liang LIU. All rights reserved.
+//
+
+#include "Simple.hpp"
+
+class Simple
+{
+    void Run();
+};
---
+++ HashCode2017/Simple.hpp
@@ -0,0 +1,24 @@
+//
+//  Simple.hpp
+//  HashCode2017
+//
+//  Created by Liang LIU on 12/02/2017.
+//  Copyright © 2017 Liang LIU. All rights reserved.
+//
+
+#ifndef Simple_hpp
+#define Simple_hpp
+
+#include <stdio.h>
+#include "InputLoader.h"
+
+void Simple::Run()
+{
+    InputLoader loader;
+    vector<vector<int>>& pizza = loader.pizza;
+    
+    vector<vector
+    
+}
+
+#endif /* Simple_hpp */

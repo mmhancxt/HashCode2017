a4cd0427ef07ab194983f149ce9beae61d667330
xiaojiao.cheng <xiaojiao.cheng@thomsonreuters.com>
2017-02-12 15:41:58 +0100

    small.in loaded

--- HashCode2017/main.cpp
+++ HashCode2017/main.cpp
@@ -7,9 +7,12 @@
 //
 
 #include <iostream>
+#include "InputLoader.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, HashCode!\n";
+	InputLoader loader;
+	loader.const_column;
     return 0;
 }

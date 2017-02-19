26a0405b3ab611a640710e92b820b0cb28b078cc
Qingfei.You <qingfeiyou@gmail.com>
2017-02-19 18:06:42 +0100

    feat(simple order): deliver simple orders

--- HashCode2017/SimpleOrder.h
+++ HashCode2017/SimpleOrder.h
@@ -6,6 +6,37 @@
 
 using namespace std;
 
+static string buildDeliveryCommand(int drone_index, int order_id, int product_id, int product_nubmer)
+{
+    return to_string(drone_index) + " " + "D" + " " +  to_string(order_id) + " " + to_string(product_id) + " " + to_string(product_nubmer);
+}
+
+static string buildLoadCommand(int drone_index, int warehouse_id, int product_id, int product_number)
+{
+    return to_string(drone_index) + " " + "L" + " " +  to_string(warehouse_id) + " " + to_string(product_id) + " " + to_string(product_number);
+}
+
+static void outputCommands(vector<string> commands)
+{
+    for (auto command : commands)
+    {
+        cout << command << endl;
+    }
+}
+
+void deleteOrder(InputLoader& input, int order_id)
+{
+    auto orders = input.orders;
+    auto it = input.orders.begin();
+    for (; it != input.orders.end(); ++it)
+    {
+        if (it->id == order_id) break;
+    }
+    if (it == input.orders.end()) return ;
+    
+    input.orders.erase(it);
+}
+
 std::vector<Order> find_one_drone_order(InputLoader& input)
 {
 	std::vector<Order>& cur_orders = input.orders;
@@ -27,3 +58,30 @@ std::vector<Order> find_one_drone_order(InputLoader& input)
 	}
 	return one_drone_order;
 }
+
+
+vector<string> loadAndDeliverSimpleOrders(InputLoader& input, vector<Order>& simple_orders)
+{
+    int drone_index = 0;
+    vector<string> commands;
+    
+    for (auto& simple_order : simple_orders)
+    {
+        for (auto& product : simple_order.purchasedProducts)
+        {
+            for (auto& warehouse : input.warehouses)
+            {
+                if (warehouse.containsProduct(product.id))
+                {
+                    warehouse.availableProducts[product.id]--;
+                    commands.emplace_back(buildLoadCommand(drone_index, warehouse.id, product.id, 1));
+                    commands.emplace_back(buildDeliveryCommand(drone_index, simple_order.id, product.id, 1));
+                    drone_index = (drone_index+1) % input.const_droneNum;
+                }
+            }
+        }
+        deleteOrder(input, simple_order.id);
+    }
+    return commands;
+}
+

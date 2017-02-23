285c4b4e5a2bd875f562a957eaf0b32e3641fac5
Qingfei.You <qingfeiyou@gmail.com>
2017-02-23 19:39:57 +0100

    feat(input): add example input data

--- HashCode2017/InputLoader.h
+++ HashCode2017/InputLoader.h
@@ -114,18 +114,61 @@ private:
 private:
     void Init()
     {
-        //const_row = 7;
-        //const_column = 6;
-        //const_min_ingredient = 1;
-        //const_max_cells_in_slice = 5;
-        //pizza = {
-        //    { 1, 0, 0, 0, 1, 1, 1 },
-        //    { 0, 0, 0, 0, 1, 0, 0 },
-        //    { 1, 1, 0, 1, 1, 0, 1 },
-        //    { 1, 0, 0, 1, 0, 0, 0 },
-        //    { 1, 1, 1, 1, 1, 1, 0 },
-        //    { 1, 1, 1, 1, 1, 1, 0 }
-        //};
+        const_video_nb = 5;
+        const_endpoints_nb = 2;
+        const_request_desc = 4;
+        const_caches_nb = 3;
+        const_cache_size = 100;
+        
+        // Videos
+        vector<int> vs = {50, 50, 80, 30, 110};
+        for (auto v : vs) {
+            Video v;
+            v.size = v;
+            videos.push_back(v);
+        }
+        
+        // Endpoints
+        EndPoint e1;
+        e1.data_center_lantency = 1000;
+        for (int i = 0; i < 3; ++i) {
+            e1.cache_ids.push_back(i);
+        }
+        e1.cache_latencies.push_back(100);
+        e1.cache_latencies.push_back(300);
+        e1.cache_latencies.push_back(200);
+        
+        EndPoint e2;
+        e2.data_center_lantency = 500;
+        
+        endpoints.push_back(e1);
+        endpoints.push_back(e2);
+        
+        // Requests
+        Request r1;
+        r1.video = 3;
+        r1.endpoint = 0;
+        r1.request_nb = 1500;
+        requests.push_back(r1);
+        
+        Request r2;
+        r2.video = 0;
+        r2.endpoint = 1;
+        r2.request_nb = 1000;
+        requests.push_back(r2);
+        
+        Request r3;
+        r3.video = 4;
+        r3.endpoint = 0;
+        r3.request_nb = 500;
+        requests.push_back(r3);
+        
+        Request r4;
+        r4.video = 1;
+        r4.endpoint = 0;
+        r4.request_nb = 1000;
+        requests.push_back(r4);
+        
     }
 
     void InitFromFile()

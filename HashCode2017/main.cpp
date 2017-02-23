e9de08cb1f0512da58a0ba96a647c29858c03d27
Zhi LI <houghstc@gmail.com>
2017-02-23 19:52:53 +0100

    input from file

--- HashCode2017/main.cpp
+++ HashCode2017/main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
 
 #include "Tools.h"
+#include "InputLoader.h"
 
 using namespace std;
 
 int main(int argc, const char * argv[])
 {
-    std::cout << "Hello, HashCode!\n";
+    //std::cout << "Hello, HashCode!\n";
 
-    for (int i = 0; i < 10; ++i)
-    {
-        cout << Tools::GetUniformRandomNumber(0, 5) << endl;
-    }
+    //for (int i = 0; i < 10; ++i)
+    //{
+    //    cout << Tools::GetUniformRandomNumber(0, 5) << endl;
+    //}
+
+    //InputLoader loader("C:\\HashCode2017\\HashCode2017\\input\\example.in");
+    InputLoader loader("C:\\HashCode2017\\HashCode2017\\input\\me_at_the_zoo.in");
+
+
+
     return 0;
 }

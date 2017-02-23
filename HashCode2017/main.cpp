a871039c62e10bce44cbcf8b5b7cb910dfdedef4
DESKTOP-58THL77\Alex <alexqian1990@gmail.com>
2017-02-23 19:57:28 +0100

    test

--- HashCode2017/main.cpp
+++ HashCode2017/main.cpp
@@ -6,38 +6,12 @@ using namespace std;
 
 
 
-void iterateEndPointMethod(InputLoader& loader) {
-	for (auto& endp : loader.endpoints) {
-		sort(endp.requests.begin(), endp.requests.end(), [&](Request r1, Request r2) { return r1.request_nb > r2.request_nb; });
-	}
-	vector<int> not_processed_request_ids;
-	not_processed_request_ids.resize(loader.endpoints.size());
-	for (auto& id : not_processed_request_ids) {
-		id = 0;
-	}
-	for (int i = 0; i < loader.endpoints.size();i++) {
-		int video_id = not_processed_request_ids[i];
-		int video_size = loader.videos[video_id];
 
-		int min_latency = 9999999999;
-		int min_available_cache = -1;
-		for (int j = 0; j < loader.endpoints[i].cache_latencies.size(); j++) {
-			if (loader.endpoints[i].cache_latencies[j] < min_latency) {
-				int cache_id = loader.endpoints[i].cache_ids[j];
-				int available_space = 
-			}
-		}
-	}
-
-}
 
 int main(int argc, const char * argv[])
 {
     std::cout << "Hello, HashCode!\n";
-
-    for (int i = 0; i < 10; ++i)
-    {
-        cout << Tools::GetUniformRandomNumber(0, 5) << endl;
-    }
+	InputLoader loader;
+	iterateEndPointMethod
     return 0;
 }
--- HashCode2017/qian.cpp
+++ HashCode2017/qian.cpp
@@ -3,26 +3,68 @@
 #include "Utilities.h"
 
 void iterateEndPointMethod(InputLoader& loader) {
-	for (auto& endp : loader.endpoints) {
-		sort(endp.requests.begin(), endp.requests.end(), [&](Request r1, Request r2) { return r1.request_nb > r2.request_nb; });
+	//Get requests per end point
+	vector<vector<Request>> request_by_endpoint;
+	request_by_endpoint.resize(loader.const_endpoints_nb);
+	for (int i = 0; i < loader.const_endpoints_nb; i++) {
+		for(int j = 0; j <loader.endpoints[i].requests.size();j++){
+			request_by_endpoint[i].push_back(loader.requests[loader.endpoints[i].requests[j]]);
+		}
 	}
+	//sort request per point
+	for (int i = 0; i < loader.const_endpoints_nb; i++) {
+		sort(request_by_endpoint[i].begin(), request_by_endpoint[i].end(), [&](Request r1, Request r2) { return r1.request_nb > r2.request_nb; });
+	}
+
+	//iterate through request ids
 	vector<int> not_processed_request_ids;
-	not_processed_request_ids.resize(loader.endpoints.size());
+	not_processed_request_ids.resize(loader.const_endpoints_nb);
 	for (auto& id : not_processed_request_ids) {
 		id = 0;
 	}
-	for (int i = 0; i < loader.endpoints.size(); i++) {
-		int video_id = not_processed_request_ids[i];
-		int video_size = loader.videos[video_id];
+	for(int round = 0; round <loader.const_request_desc;round++){
+		cout << "round " << round << endl;
+		for (int i = 0; i < loader.endpoints.size(); i++) {
+			int not_processed_request_id = not_processed_request_ids[i];
+			if (not_processed_request_id == request_by_endpoint[i].size() - 1)
+				continue;
+			int video_id = request_by_endpoint[i][not_processed_request_id].video;
 
-		int min_latency = 9999999999;
-		int min_available_cache = -1;
-		for (int j = 0; j < loader.endpoints[i].cache_latencies.size(); j++) {
-			if (loader.endpoints[i].cache_latencies[j] < min_latency) {
-				int cache_id = loader.endpoints[i].cache_ids[j];
-				int available_space = cacheAvailableSpace(loader.caches[cache_id]);
+			int video_size = loader.videos[video_id].size;
+
+			int min_latency = 9999999999;
+			int min_available_cache = -1;
+			for (int j = 0; j < loader.endpoints[i].cache_latencies.size(); j++) {
+				if (loader.endpoints[i].cache_latencies[j] < min_latency) {
+					int cache_id = loader.endpoints[i].cache_ids[j];
+					int available_space = cacheAvailableSpace(loader.caches[cache_id]);
+					if (available_space >= video_size) {
+						min_available_cache = j;
+					}
+				}
 			}
+			if (min_available_cache != -1) {
+				loader.caches[min_available_cache].videos.push_back(video_id);
+			}
+			not_processed_request_ids[i]++;
 		}
 	}
+	int count = 0;
+	for (int i = 0; i < loader.const_caches_nb; i++) {
+		if (loader.caches[i].videos.size() > 0) {
+			count++;
+		}
 
+	}
+	cout << count;
+	for (int i = 0; i < loader.const_caches_nb; i++) {
+		string str;
+		str += to_string(i);
+		str+=" ";
+		for (int j = 0; j < loader.caches[i].videos.size(); j++) {
+			str += loader.caches[i].videos[j];
+			str += " ";
+		}
+		cout << str << endl;
+	}
 }
